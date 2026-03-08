3776de4718ea71f02a025560d132a94149ff55ae
Taqi Tazwar <189677955+taqitazwar@users.noreply.github.com>
2026-03-08 04:37:40 -0230

    export all constants from constants index

---
+++ src/constants/index.ts
@@ -0,0 +1,5 @@
+export { default as Colors } from './colors';
+export { default as Typography } from './typography';
+export { default as Spacing } from './spacing';
+export { default as Layout } from './layout';
+export { default as GameConfig } from './game';
