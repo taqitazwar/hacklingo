daba54a3bc0e631038064a37446d592bee3093e1
Taqi Tazwar <189677955+taqitazwar@users.noreply.github.com>
2026-03-08 04:44:36 -0230

    export all challenge components from challenges index

---
+++ src/components/challenges/index.ts
@@ -0,0 +1,6 @@
+export { default as PredictOutputChallenge } from './PredictOutputChallenge';
+export { default as MultipleChoiceChallenge } from './MultipleChoiceChallenge';
+export { default as FillBlankChallenge } from './FillBlankChallenge';
+export { default as FixBugChallenge } from './FixBugChallenge';
+export { default as ReorderBlocksChallenge } from './ReorderBlocksChallenge';
+export { default as OptionButton } from './OptionButton';
