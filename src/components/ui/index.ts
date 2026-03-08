75e6d9cfead3d1505f5a0a775e7a8bde92dcaf65
Taqi Tazwar <189677955+taqitazwar@users.noreply.github.com>
2026-03-08 04:44:16 -0230

    export all UI components from ui index

---
+++ src/components/ui/index.ts
@@ -0,0 +1,5 @@
+export { default as Button } from './Button';
+export { default as ProgressBar } from './ProgressBar';
+export { default as CodeBlock } from './CodeBlock';
+export { default as HeartsDisplay } from './HeartsDisplay';
+export { default as XPBadge } from './XPBadge';
